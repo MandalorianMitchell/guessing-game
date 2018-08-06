45dd280c5c129fd082b01825e20dd948e8ad77ba
MandalorianMitchell <haynesmitchell@gmail.com>
2018-08-06 07:56:27 -0700

    Final tweaks to my guessing-game

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # guessing-game
-This is the project for Alchemy Code Lab Week 1 Class 2 - Guessing Game
+<h1>This is the project for Alchemy Code Lab Week 1 Class 2 - Guessing Game</h1>
+<br>
+<p>I have included information about myself in the HTML portion of my Guessing Game that users can read in order to answer each question.
+<br>
+<p>Questions -</p>
+    <ul>
+      <li>Number 1: Was I born in Oregon?
+      <li>Number 2: How many cats do I have?
+      <li>Number 3: Is my favorite color yellow?
+      <li>Number 4: What is my favorite number?
+      <li>Bonus Question: Guess a number between 1 and 50.
+    </ul>
+<br>
+<p>I have included a button after my About Me section that, when clicked, asks the user for their name, and then prints their name on the webpage. I have also added buttons that coincide with the questions I have asked, and when those are clicked, these buttons will prompt the user with a question. When answered, it will print the user's answer into the webpage in either green or red text (depending on right/wrong answer), and with an image of either a thumbs up or thumbs down (depending on right/wrong answer).</p>
+<br>
+<p>Once the user answers all questions, there is another button at the bottom of the page that the user can click to see what their score was!</p>
---
+++ main.js
@@ -0,0 +1,100 @@
+function beginGame() {
+  var userName = prompt('Before we get started, please enter your name:');
+  while ((userName == '') || (userName == null))  {
+    userName = prompt('Please enter a valid name:');
+  }
+
+  var iKnowYou = document.getElementById('who-are-you-?');
+  iKnowYou.innerHTML = 'Hello ' + userName + ', let\'s get started!';
+  console.log('Username: ' + userName);
+}
+
+var counter = 0;
+function askQuestion(question, correctAnswer, elementId, correctResponse, wrongResponse) {
+  var userAnswer = confirm(question);
+  var p = document.getElementById(elementId);
+  if (userAnswer === correctAnswer) {
+    p.innerHTML = '<span class="color-green">' + correctResponse + '</span>';
+    console.log(userAnswer + ': Correct');
+    counter++;
+    console.log('Score: ' + counter);
+  } else {
+    document.getElementById(elementId);
+    p.innerHTML = '<span class="color-red">' + wrongResponse + '<span>';
+    console.log(userAnswer + ': Wrong');
+  }
+}
+
+function oregonBorn() {
+  askQuestion('Was I born in Oregon?', false, 'birth-place',
+   'Correct! I was not born in Oregon.' +
+   '<br><br><img src="./Images/Thumbs-up.jpg" alt="John Rambo" width="200px">',
+   'Wrong! I was born in California.' +
+   '<br><br><img src="./Images/Thumbs-down.jpg" alt="Thumbs down guy" width="200px">');
+}
+
+function lotsOfCats(meow) {
+  var questionTwo = prompt(meow);
+  var q = document.getElementById('pet-count');
+  if (questionTwo == 2) {
+    q.innerHTML = '<span class="color-green">' + 'Hey! You got it!' + '<span>' +
+    '<br><br><img src="./Images/JCVD-Thumbs-up.jpg" alt="JCVD" width="200px">';
+    console.log(meow + ': Correct');
+    counter++;
+    console.log('Score: ' + counter);
+  } else {
+    q.innerHTML = '<span class="color-red">' + 'Sorry, that wasn\'t the right answer!' + '<span>' +
+    '<br><br><img src="./Images/Kevin-Thumbs-down.jpg" alt="Kevin from The Office" width="200px">';
+    console.log(meow + ': Wrong');
+  }
+}
+
+function faveColor() {
+  askQuestion('Is my favorite color Yellow?', true, 'i-like-this-color',
+   'Yeah! You got it right!' +
+   '<br><br><img src="./Images/Borat-Thumbs-up.jpg" alt="Borat" width="200px">',
+   'You got it wrong, bummer!' +
+   '<br><br><img src="./Images/Sad-James-Vanderbeek.jpg" alt="Dawson\'s Creek" width="200px">');
+}
+
+function coolNumber(number) {
+  var bonusQuestion = prompt(number);
+  var s = document.getElementById('this-number-is-rad');
+  if (bonusQuestion == 14) {
+    s.innerHTML = '<span class="color-green">' + 'You guessed it! That was super impressive!' + '<span>' +
+    '<br><br><img src="./Images/Arnold-Thumbs-up.jpg" alt="Terminator thumbs up" width="200px">';
+    console.log(number + ': Correct');
+    counter++;
+    console.log('Score: ' + counter);
+  } else if (bonusQuestion < 14) {
+    s.innerHTML = '<span class="color-red">' + 'Sorry, that answer was too low!' + '<span>' +
+    '<br><br><img src="./Images/Wrong-Answer.jpg" alt="Wrong Answer" width="200px">';
+    console.log(number + ': Wrong - answer too low!');
+  } else if (bonusQuestion > 14) {
+    s.innerHTML = '<span class="color-red">' + 'Sorry, that answer was too high!' + '<span>' +
+    '<br><br><img src="./Images/Wrong-Answer.jpg" alt="Wrong Answer" width="200px">';
+    console.log(number + ': Wrong - answer too high!');
+  }
+}
+
+function mindReader(psychic) {
+  var questionFive = prompt(psychic);
+  var t = document.getElementById('try-and-guess');
+  while ((questionFive == '') || (questionFive != 38)) {
+    questionFive = prompt('Sorry, that answer was wrong. Please try again.');
+    console.log(psychic + ': Wrong answer! Keep Trying!');
+  }
+
+  if (questionFive) {
+    t.innerHTML = '<span class="color-green">' + 'You guessed it! Awesome!' + '<span>' +
+    '<br><br><img src="./Images/Hasselhoff-Thumbs-up.jpg" alt="David Hasselhoff" width="200px">';
+    console.log(psychic + ': Correct - great guess!');
+    counter++;
+    console.log('Score: ' + counter);
+  }
+}
+
+function gameScore() {
+  alert('You scored ' + counter + '/5 Questions Correctly');
+  console.log('You scored ' + counter + '/5 Questions Correctly');
+}
